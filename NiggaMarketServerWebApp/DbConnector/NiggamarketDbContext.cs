30368be39dbc00878915ae278c5bb3ed888624b4
itpark <a.m.smyslov@yandex.ru>
2023-07-23 13:29:08 +0300

    fix for docker

--- NiggaMarketServerWebApp/DbConnector/NiggamarketDbContext.cs
+++ NiggaMarketServerWebApp/DbConnector/NiggamarketDbContext.cs
@@ -28,9 +28,11 @@ public partial class NiggamarketDbContext : DbContext
 
     public virtual DbSet<Work> Works { get; set; }
 
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //     => optionsBuilder.UseNpgsql("Host=194.67.105.79:5432;Database=niggamarket_db;Username=niggamarket_user;Password=12345");
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=194.67.105.79:5432;Database=niggamarket_db;Username=niggamarket_user;Password=12345");
+        => optionsBuilder.UseNpgsql("Host=postgres:5432;Database=postgres;Username=postgres;Password=postgres");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
